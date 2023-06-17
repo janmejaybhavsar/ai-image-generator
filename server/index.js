6bccf882228b76f5b66b9528bf83bfc4e5293084
janmejaybhavsar <89321629+janmejaybhavsar@users.noreply.github.com>
2023-06-17 12:57:31 -0700

    Create server and completed CreatePost Form

---
+++ server/index.js
@@ -0,0 +1,25 @@
+import express from 'express';
+import * as dotenv from 'dotenv';
+import cors from 'cors';
+
+dotenv.config();
+const app = express();
+app.use(cors());
+app.use(express.json({limit: '50mb'}));
+
+app.get('/', async (req, res) => {
+    res.send('Hello From DALLE!');
+    }
+);
+
+const startServer = async () => {
+    try {
+        app.listen(8080, () => {
+            console.log(`Server listening on port http://localhost:8080`);
+        });
+    } catch (err) {
+        console.log(err);
+    }
+}
+
+startServer();
